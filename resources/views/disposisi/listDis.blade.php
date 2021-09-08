08b2cc3f9a478edfcaa3f50b8cb2503b3ded332e
jajang <jajang.jaenudin.12.210@gmail.com>
2021-09-08 12:28:27 +0700

    upload lagi

--- resources/views/disposisi/listDis.blade.php
+++ resources/views/disposisi/listDis.blade.php
@@ -79,7 +79,41 @@
                     </div>
                   </div><!-- /.container-fluid -->
                 </section>
-
+                <section class="content">
+                  <div class="container-fluid">
+                      
+                      <form action="enhanced-results.html">
+                          <div class="row">
+                              <div class="col-md-10">
+                                  
+                                      <div class="col-6">
+                                          <!-- <div class="form-group">
+                                            <div class="input-group input-group-lg">
+                                              <label>Order By:</label>
+                                              <select class="form-control select2" style="width: 100%;"  >
+                                                  <option selected>Title</option>
+                                                  <option>Date</option>
+                                              </select>
+                                            </div>
+                                          </div> -->
+                                          <div class="form-group">
+                                          <div class="input-group input-group-lg">
+                                              <input type="search" class="form-control form-control-lg" placeholder="Type your keywords here" value="Lorem ipsum">
+                                              <div class="input-group-append">
+                                                  <button type="submit" class="btn btn-lg btn-default">
+                                                      <i class="fa fa-search"></i>
+                                                  </button>
+                                              </div>
+                                          </div>
+                                        
+                                      </div>
+                                  </div>
+                                  
+                              </div>
+                          </div>
+                      </form>
+                  </div>
+              </section>
 
               <!-- /.card-header -->
               <div class="card-body">
